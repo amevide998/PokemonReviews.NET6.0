2b61413c06416c76c51e09ea3f06b9b33695b157
amevide998 <hdsvidi@gmail.com>
2023-09-09 18:46:07 +0700

    add dbcontext, connection to supabase and seed

--- PokemonReviews/Models/Category.cs
+++ PokemonReviews/Models/Category.cs
@@ -6,5 +6,6 @@ namespace PokemonReviews.Models
     {
         public int Id { get; set; }
         public string? Name { get; set; }
+        public ICollection<PokemonCategory>? PokemonCategories { get; set; }
     }
 }
--- PokemonReviews/Models/Owner.cs
+++ PokemonReviews/Models/Owner.cs
@@ -5,8 +5,10 @@ namespace PokemonReviews.Models
     public class Owner
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
         public string? Gym { get; set; }
         public Country? Country { get; set; }
+        public ICollection<PokemonOwner>? PokemonOwners { get; set; }
     }
 }
